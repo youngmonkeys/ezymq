65a9523d22c7afbfe2243d89f216e8b132184180
Dung Ta Van <itprono3@gmail.com>
2023-07-20 22:44:33 +0700

    update EzyActiveMQProxyIT

--- ezymq-activemq/src/test/java/com/tvd12/ezymq/activemq/test/EzyActiveMQProxyIT.java
+++ ezymq-activemq/src/test/java/com/tvd12/ezymq/activemq/test/EzyActiveMQProxyIT.java
@@ -2,6 +2,7 @@ package com.tvd12.ezymq.activemq.test;
 
 import com.tvd12.ezymq.activemq.EzyActiveMQProxy;
 import com.tvd12.ezymq.activemq.EzyActiveRpcProducer;
+import com.tvd12.ezymq.activemq.EzyActiveTopic;
 import com.tvd12.ezymq.activemq.handler.EzyActiveRequestHandler;
 import com.tvd12.ezymq.activemq.setting.EzyActiveSettings;
 
@@ -12,6 +13,7 @@ public class EzyActiveMQProxyIT {
     public EzyActiveMQProxyIT() {
         EzyActiveSettings settings = EzyActiveSettings.builder()
             .mapRequestType("fibonacci", Integer.class)
+            .mapTopicMessageType("test", "hello", String.class)
             .rpcProducerSettingBuilder("test")
             .defaultTimeout(300 * 1000)
             .requestQueueName("rpc-request-test-1")
@@ -29,6 +31,11 @@ public class EzyActiveMQProxyIT {
                     }
                 })
             .parent()
+            .topicSettingBuilder("test")
+            .consumerEnable(true)
+            .producerEnable(true)
+            .topicName("test")
+            .parent()
             .build();
         this.activeMQProxy = EzyActiveMQProxy.builder()
             .settings(settings)
@@ -38,12 +45,18 @@ public class EzyActiveMQProxyIT {
     public void test() {
         EzyActiveRpcProducer producer = activeMQProxy.getRpcProducer("test");
         long start = System.currentTimeMillis();
-        for (int i = 0; i < 1000; ++i) {
+        for (int i = 0; i < 100; ++i) {
             System.out.println("rabbit rpc start call: " + i);
             int result = producer.call("fibonacci", 100, int.class);
             System.out.println("i = " + i + ", result = " + result);
         }
         System.out.println("elapsed = " + (System.currentTimeMillis() - start));
+
+        EzyActiveTopic<String> topic = activeMQProxy.getTopic("test");
+        topic.addConsumer("hello", it ->
+            System.out.println("topic received: " + it)
+        );
+        topic.publish("hello", "Young Monkeys");
     }
 
     public static void main(String[] args) throws Exception {
